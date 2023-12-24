d1ef719e8215abb463708648fe4301ec350f3707
J-Lw <JLaw0@proton.me>
2023-12-24 15:14:44 -0400

    checkpoint.

--- crypto/src/ascii.rs
+++ crypto/src/ascii.rs
@@ -1,1 +1,3 @@
-
+// p:
+// - Using the following integer array, convert the numbers to their corresponding ascii characters to obtain a flag.
+// - [99, 114, 121, 112, 116, 111, 123, 65, 83, 67, 73, 73, 95, 112, 114, 49, 110, 116, 52, 98, 108, 51, 125].

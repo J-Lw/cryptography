0462e1d72d551b88748b4d28a58294e78a182c78
J-Lw <JLaw0@proton.me>
2024-01-06 15:35:40 -0400

    Checkpoint.

--- crypto/src/ascii.rs
+++ crypto/src/ascii.rs
@@ -1,3 +1,25 @@
 // p:
 // - Using the following integer array, convert the numbers to their corresponding ascii characters to obtain a flag.
 // - [99, 114, 121, 112, 116, 111, 123, 65, 83, 67, 73, 73, 95, 112, 114, 49, 110, 116, 52, 98, 108, 51, 125].
+pub fn to_ascii(numbers: Vec<i32>) -> String {
+    let mut flag = String::new();
+     
+    for number in numbers.iter() {
+        if number <= 57 {
+            number - 48;
+            flag.append(number);
+        }
+    }
+
+    flag
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        println!("{}", to_ascii(vec![99, 114, 121, 112, 116, 111, 123, 65, 83, 67, 73, 73, 95, 112, 114, 49, 110, 116, 52, 98, 108, 51, 125]));
+    }
+}
